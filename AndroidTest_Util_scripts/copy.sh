5962ff68ac4b01f34d9aca964dcb52057d856f1f
bastin2286 <56135793+bastin2286@users.noreply.github.com>
2021-03-17 17:21:22 +0530

    Add files via upload

---
+++ AndroidTest_Util_scripts/copy.sh
@@ -0,0 +1,14 @@
+#echo Google@2020 | sudo -S cp -R /var/lib/docker/volumes/taacVolume/_data/workspace/Android_Test/Android_Demo /var/lib/docker/volumes/volAndroidTest/_data/
+
+echo Google@2020 | sudo -S rm -rf /home/318356/AndroidTest
+
+
+git clone https://github.com/bastin2286/AndroidTest.git
+
+
+echo Google@2020 | sudo -S cp -R /home/318356/AndroidTest /var/lib/docker/volumes/volAndroidTest/_data/
+
+
+echo Google@2020 | sudo -S cp /home/318356/Android_Test_Backup/Android_Demo/tcs_lib_Jars/tcs_taac_android_lib.jar /var/lib/docker/volumes/volAndroidTest/_data/AndroidTest/Test_Mobile_Andriod_demo_docker/tcs_lib_Jars
+
+
---
+++ AndroidTest_Util_scripts/copy_xml.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo Google@2020 | sudo -S cp /var/lib/docker/volumes/volAndroidTest/_data/AndroidTest/Test_Mobile_Andriod_demo_docker/target/surefire-reports/testng-results.xml /var/lib/docker/volumes/taacVolume/_data/workspace/Mobile_Android_Test/
+
